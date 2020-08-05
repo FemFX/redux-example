1f244572ee29ed12b9ec2c9105a2aa738b6df2ad
FemFX <ignedko34@gmail.com>
2020-08-05 09:38:46 +0300

    Create Posts.jsx

---
+++ src/components/Posts.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Post from './Post';
+
+const Posts = ({ posts }) => {
+  if (!posts.length) {
+    return <p className="text-center">Постов пока нет</p>;
+  }
+  return posts.map((post) => <Post key={post.id} post={post} />);
+};
+const mapStateToProps = (state) => {
+  return { posts: state.posts.posts };
+};
+export default connect(mapStateToProps)(Posts);
