545e4e373e252538a43af61b9b2c3ef150892779
FemFX <ignedko34@gmail.com>
2020-08-05 09:38:56 +0300

    Create Loader.jsx

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loader = () => (
+  <div>
+    <div className="spinner-border text-danger" role="status">
+      <span className="sr-only">Loading</span>
+    </div>
+  </div>
+);
+
+export default Loader;
